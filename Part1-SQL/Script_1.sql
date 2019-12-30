3d1b0ee1ae5e6819b9d0b3cafd437139de76aa13
gpapavgeri <gpapavgeri@outlook.com>
2019-12-31 00:25:52 +0200

    Part1 updated

--- Part1-SQL/Script_1.sql
+++ Part1-SQL/Script_1.sql
@@ -1,43 +1,61 @@
 use dbo;
 
+-- BEFORE THE EXECUTION OF THE STORED PROCEDURE
+-- Show Employees who already have the attribute 'Weight' but NOT with value 'Thin' and need to be updated
+-- [Greg, Phil]
+SELECT e.EMP_Name, a.ATTR_Name, a.ATTR_Value
+FROM employee e
+INNER JOIN employeeattribute ea ON ea.EMPATTR_EmployeeID = e.EMP_ID
+INNER JOIN attribute a ON ea.EMPATTR_AttributeID = a.ATTR_ID
+WHERE a.ATTR_Name = 'Weight' AND a.ATTR_Value != 'Thin'
+ORDER BY e.EMP_Name;
+
+-- Show Employees who already have the attribute 'Weight' with value 'Thin'
+-- [Oleg]
+SELECT e.EMP_Name, a.ATTR_Name, a.ATTR_Value
+FROM employee e
+INNER JOIN employeeattribute ea ON ea.EMPATTR_EmployeeID = e.EMP_ID
+INNER JOIN attribute a ON ea.EMPATTR_AttributeID = a.ATTR_ID
+WHERE a.ATTR_Name = 'Weight' and a.ATTR_Value='Thin'
+ORDER BY e.EMP_Name;
+
 DELIMITER //
 CREATE PROCEDURE ADD_ATTRIBUTE_TO_ALL_EMPLOYEES (IN attributeName VARCHAR(50), IN attributeValue VARCHAR(50))
 BEGIN
-DECLARE FINISHED BOOLEAN DEFAULT 0; -- ΔΙΝΟΥΜΕ DEFAULT ΤΙΜΗ 0 ΔΗΛΑΔΗ FALSE
-	DECLARE EMP_ID CHAR(38);
+DECLARE FINISHED BOOLEAN DEFAULT 0; 
+	DECLARE EMP_ID BINARY(16);
     DECLARE EMP_Name VARCHAR(100);
-    DECLARE EMP_DateOfHire DATETIME;
-    DECLARE EMP_Supervisor CHAR(38);
+    DECLARE EMP_DateOfHire TIMESTAMP;
+    DECLARE EMP_Supervisor BINARY(16);
     DECLARE EMP_CURSOR CURSOR FOR 
     SELECT * FROM employee;
-    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET FINISHED = 1; -- TO 1 EINAI TRUE OTAN ΔΕΝ ΕΧΕΙ ΑΛΛΗ ΤΙΜΗ
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET FINISHED = 1;
     OPEN EMP_CURSOR;
     START TRANSACTION;
     WHILE NOT FINISHED DO
 		FETCH EMP_CURSOR INTO EMP_ID, EMP_Name, EMP_DateOfHire, EMP_Supervisor;
         
         IF (SELECT EXISTS (SELECT * FROM EmployeeAttribute 
-							WHERE EMPATTR_EmployeeID = EMP_ID AND
-                            EMPATTR_AttributeID IN (
+					WHERE EMPATTR_EmployeeID = EMP_ID AND
+						  EMPATTR_AttributeID IN (
 								SELECT ATTR_ID 
 								FROM Attribute 
 								WHERE ATTR_Name = attributeName))) THEN
-        
 			UPDATE EmployeeAttribute
-			SET EMPATTR_AttributeID = (SELECT ATTR_ID FROM Attribute
-					WHERE ATTR_Name = attributeName AND ATTR_Value = attributeValue)
+			SET EMPATTR_AttributeID = (SELECT ATTR_ID 
+								FROM Attribute
+								WHERE ATTR_Name = attributeName AND ATTR_Value = attributeValue)
 			WHERE EMPATTR_EmployeeID = EMP_ID AND
-                            EMPATTR_AttributeID IN (
+				  EMPATTR_AttributeID IN (
 								SELECT ATTR_ID 
 								FROM Attribute 
 								WHERE ATTR_Name = attributeName);
-            
         ELSE
 			INSERT INTO employeeattribute VALUES (
 				EMP_ID, 
-				(SELECT ATTR_ID FROM Attribute
+				(SELECT ATTR_ID 
+                FROM Attribute
 				WHERE ATTR_Name = attributeName AND ATTR_Value = attributeValue));
-		
         END IF;
         
         END WHILE;
@@ -47,6 +65,25 @@ DECLARE FINISHED BOOLEAN DEFAULT 0; -- ΔΙΝΟΥΜΕ DEFAULT ΤΙΜΗ 0 ΔΗΛ�
 END //
 DELIMITER ;
 
+-- EXECUTE THE STORED PROCEDURE
 CALL ADD_ATTRIBUTE_TO_ALL_EMPLOYEES('Weight', 'Thin');
-SELECT * FROM EmployeeAttribute
-ORDER BY EMPATTR_EmployeeID;
+
+-- AFTER THE EXECUTION OF THE STORED PROCEDURE
+-- Show Employees who already have the attribute 'Weight' but NOT with value 'Thin' 
+-- [None] 
+-- Both of the Employees [Greg, Phil] who had the attribute 'Weight' with a different value now are updated
+SELECT e.EMP_Name, a.ATTR_Name, a.ATTR_Value
+FROM employee e
+INNER JOIN employeeattribute ea ON ea.EMPATTR_EmployeeID = e.EMP_ID
+INNER JOIN attribute a ON ea.EMPATTR_AttributeID = a.ATTR_ID
+WHERE a.ATTR_Name = 'Weight' AND a.ATTR_Value != 'Thin'
+ORDER BY e.EMP_Name;
+
+-- Show Employees who have the attribute 'Weight' with value 'Thin'
+-- [ALL THE EMPLOYEES]
+SELECT e.EMP_Name, a.ATTR_Name, a.ATTR_Value
+FROM employee e
+INNER JOIN employeeattribute ea ON ea.EMPATTR_EmployeeID = e.EMP_ID
+INNER JOIN attribute a ON ea.EMPATTR_AttributeID = a.ATTR_ID
+WHERE a.ATTR_Name = 'Weight' and a.ATTR_Value='Thin'
+ORDER BY e.EMP_Name;
--- Part1-SQL/Script_2.sql
+++ Part1-SQL/Script_2.sql
@@ -1,13 +1,47 @@
 use dbo;
 
+-- BEFORE THE EXECUTION OF THE STORED PROCEDURE
+-- Show All the Supervisors
+-- [Greg, Oleg] 
+SELECT EMP_Name
+FROM employee
+WHERE EMP_ID IN (
+SELECT DISTINCT EMP_Supervisor
+FROM employee
+WHERE EMP_Supervisor IS NOT NULL);
+
+-- Show Supervisors who already have the attribute 'Height' but NOT with value 'Short' and need to be updated
+-- [Greg]
+SELECT e.EMP_Name, a.ATTR_Name, a.ATTR_Value
+FROM employee e
+INNER JOIN employeeattribute ea ON ea.EMPATTR_EmployeeID = e.EMP_ID
+INNER JOIN attribute a ON ea.EMPATTR_AttributeID = a.ATTR_ID
+WHERE a.ATTR_Name = 'Height' AND a.ATTR_Value != 'Short' AND e.EMP_ID IN (
+	SELECT DISTINCT EMP_Supervisor
+	FROM employee
+	WHERE EMP_Supervisor IS NOT NULL)
+ORDER BY e.EMP_Name;
+
+-- Show Supervisors who already have the attribute 'Height' with value 'Short'
+-- [Oleg]
+SELECT e.EMP_Name, a.ATTR_Name, a.ATTR_Value
+FROM employee e
+INNER JOIN employeeattribute ea ON ea.EMPATTR_EmployeeID = e.EMP_ID
+INNER JOIN attribute a ON ea.EMPATTR_AttributeID = a.ATTR_ID
+WHERE a.ATTR_Name = 'Height' AND a.ATTR_Value = 'Short' AND e.EMP_ID IN (
+	SELECT DISTINCT EMP_Supervisor
+	FROM employee
+	WHERE EMP_Supervisor IS NOT NULL)
+ORDER BY e.EMP_Name;
+
 DELIMITER //
 CREATE PROCEDURE ADD_ATTRIBUTE_TO_SUPERVISORS (IN attributeName VARCHAR(50), IN attributeValue VARCHAR(50))
 BEGIN
 DECLARE FINISHED BOOLEAN DEFAULT 0; 
-	DECLARE EMP_ID CHAR(38);
+	DECLARE EMP_ID BINARY(16);
     DECLARE EMP_Name VARCHAR(100);
-    DECLARE EMP_DateOfHire DATETIME;
-    DECLARE EMP_Supervisor CHAR(38);
+    DECLARE EMP_DateOfHire TIMESTAMP;
+    DECLARE EMP_Supervisor BINARY(16);
     DECLARE EMP_CURSOR CURSOR FOR 
     SELECT * FROM employee;
     DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET FINISHED = 1; 
@@ -17,35 +51,40 @@ DECLARE FINISHED BOOLEAN DEFAULT 0;
 		FETCH EMP_CURSOR INTO EMP_ID, EMP_Name, EMP_DateOfHire, EMP_Supervisor;
         
         IF (SELECT EXISTS (SELECT * FROM EmployeeAttribute 
-						   WHERE (EMPATTR_EmployeeID IN (
-									SELECT EMP_Supervisor IS NOT NULL
-									FROM Employee) AND
-								 EMPATTR_AttributeID IN (
-									SELECT ATTR_ID 
-									FROM Attribute 
-									WHERE ATTR_Name = attributeName))
-							)) THEN
+					WHERE (EMPATTR_EmployeeID IN (
+								SELECT DISTINCT EMP_Supervisor
+								FROM employee
+								WHERE EMP_Supervisor IS NOT NULL) AND
+							EMPATTR_AttributeID IN (
+								SELECT ATTR_ID 
+								FROM Attribute 
+								WHERE ATTR_Name = attributeName))
+					)) THEN
 			UPDATE EmployeeAttribute
 			SET EMPATTR_AttributeID = (
-					SELECT ATTR_ID FROM Attribute
+					SELECT ATTR_ID 
+                    FROM Attribute
 					WHERE (ATTR_Name = attributeName AND ATTR_Value = attributeValue)
                     LIMIT 1)
 			WHERE (EMPATTR_EmployeeID IN (
-						SELECT EMP_Supervisor IS NOT NULL
-						FROM Employee) AND
+						SELECT DISTINCT EMP_Supervisor
+						FROM employee
+						WHERE EMP_Supervisor IS NOT NULL) AND
 				   EMPATTR_AttributeID IN (
 						SELECT ATTR_ID 
 						FROM Attribute 
 						WHERE ATTR_Name = attributeName));
             
         ELSEIF (SELECT EXISTS (SELECT EMP_ID FROM Employee 
-								WHERE (EMP_ID IN (
-									SELECT EMP_Supervisor IS NOT NULL
-									FROM Employee))
-								)) THEN
+						WHERE (EMP_ID IN (
+							SELECT DISTINCT EMP_Supervisor
+							FROM employee
+							WHERE EMP_Supervisor IS NOT NULL))
+						)) THEN
 			INSERT INTO employeeattribute VALUES (
 					EMP_ID, 
-					(SELECT ATTR_ID FROM Attribute
+					(SELECT ATTR_ID 
+                    FROM Attribute
 					WHERE (ATTR_Name = attributeName AND ATTR_Value = attributeValue)
                     LIMIT 1));
         END IF;
@@ -57,7 +96,32 @@ DECLARE FINISHED BOOLEAN DEFAULT 0;
 END //
 DELIMITER ;
 
+-- EXECUTE THE STORED PROCEDURE
 CALL ADD_ATTRIBUTE_TO_SUPERVISORS('Height', 'Short');
-SELECT * FROM EmployeeAttribute
-ORDER BY EMPATTR_EmployeeID;
+
+-- AFTER THE EXECUTION OF THE STORED PROCEDURE
+-- Show Supervisors who already have the attribute 'Height' but NOT with value 'Short' 
+-- [None]
+-- The supervisor [Greg] who had the attribute 'Height' with a different value now is updated
+SELECT e.EMP_Name, a.ATTR_Name, a.ATTR_Value
+FROM employee e
+INNER JOIN employeeattribute ea ON ea.EMPATTR_EmployeeID = e.EMP_ID
+INNER JOIN attribute a ON ea.EMPATTR_AttributeID = a.ATTR_ID
+WHERE a.ATTR_Name = 'Height' AND a.ATTR_Value != 'Short' AND e.EMP_ID IN (
+	SELECT DISTINCT EMP_Supervisor
+	FROM employee
+	WHERE EMP_Supervisor IS NOT NULL)
+ORDER BY e.EMP_Name;
+
+-- Show Supervisors who have the attribute 'Height' with value 'Short'
+-- [Greg, Oleg / All the Supervisors]
+SELECT e.EMP_Name, a.ATTR_Name, a.ATTR_Value
+FROM employee e
+INNER JOIN employeeattribute ea ON ea.EMPATTR_EmployeeID = e.EMP_ID
+INNER JOIN attribute a ON ea.EMPATTR_AttributeID = a.ATTR_ID
+WHERE a.ATTR_Name = 'Height' AND a.ATTR_Value = 'Short' AND e.EMP_ID IN (
+	SELECT DISTINCT EMP_Supervisor
+	FROM employee
+	WHERE EMP_Supervisor IS NOT NULL)
+ORDER BY e.EMP_Name;
 
